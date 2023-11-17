e90645b8cb8ba434c42fc866a96026018bda3c1b
ksnedeker1 <snedekek@oregonstate.edu>
2023-11-17 03:15:45 -0500

    Initial commit

---
+++ database/dmq.py
@@ -0,0 +1,18 @@
+r_power_source_types = 'SELECT * FROM PowerSourceTypes'
+r_power_sources = 'SELECT * FROM PowerSources'
+r_substation_types = 'SELECT * FROM SubstationTypes'
+r_substations = 'SELECT * FROM Substations'
+r_cities = 'SELECT * FROM Cities'
+r_city_hqs = 'SELECT * FROM CityHQs'
+r_local_generator_types = 'SELECT * FROM LocalGeneratorTypes'
+r_local_generators = 'SELECT * FROM LocalGenerators'
+r_power_source_substation_links = 'SELECT * FROM PowerSourceSubstationLinks'
+r_city_substation_links = 'SELECT * FROM CitySubstationLinks'
+
+r_city_substation_link = 'SELECT * FROM CitySubstationLinks WHERE linkID = %s'
+
+d_city_substation_link = 'DELETE FROM CitySubstationLinks WHERE linkID = %s'
+
+c_city_substation_link = 'INSERT INTO CitySubstationLinks (cityID, substationID) VALUES (%s, %s)'
+
+u_city_substation_link = 'UPDATE CitySubstationLinks SET substationID = %s, cityID = %s WHERE linkID = %s'
